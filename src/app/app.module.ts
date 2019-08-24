033b9405d29898bdee43a326839d203cfb92dd47
netrometro <netrometro@yahoo.com.br>
2019-08-24 15:20:01 -0300

    Component card.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './cards/card/card.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
