8addeb812f2f685d5530b72f73456b66350901ae
netrometro <netrometro@yahoo.com.br>
2019-08-25 10:31:51 -0300

    Style no primeng =....(  { Bem sofrido }

--- src/app/countrystatus/countrystatus.component.ts
+++ src/app/countrystatus/countrystatus.component.ts
@@ -12,17 +12,9 @@ import { MenuItem } from 'primeng/api';
 })
 export class CountrystatusComponent implements OnInit {
     
-  items: MenuItem[];
-  activeItem: MenuItem;
+  culture: number[];
 
   ngOnInit() {
-    this.items = [
-      {label: 'Stats', icon: 'fa fa-fw fa-bar-chart'},
-      {label: 'Calendar', icon: 'fa fa-fw fa-calendar'},
-      {label: 'Documentation', icon: 'fa fa-fw fa-book'},
-      {label: 'Support', icon: 'fa fa-fw fa-support'},
-      {label: 'Social', icon: 'fa fa-fw fa-twitter'}
-    ];
-    this.activeItem = this.items[2];
+    
   }
 }
