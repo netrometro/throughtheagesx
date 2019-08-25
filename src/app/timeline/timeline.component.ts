28cfc9f35cc3f3eb1fe8ce05b6427cc2198584b7
netrometro <netrometro@yahoo.com.br>
2019-08-25 16:21:14 -0300

    Accordion na timeline para ganhar espaço.

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TimelineComponent implements OnInit {
 
